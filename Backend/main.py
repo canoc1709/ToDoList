ecd58f96b115a6db7a4e7a39bc4e9e91e102f265
for2024 <phamchung1.qtkd@gmail.com>
2024-02-05 01:52:55 +0700

    them fetch nhung chua phu hop yeu cau

--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+import json
+import os 
 app = Flask(__name__)
 
 CORS(app)
 
-@app.route('/')
-def api():
-    return jsonify({'data':'Hello, World!'})
+
+@app.route('/getData', methods = ['GET'])
+def get():
+    return render_template("index2.html")
+
