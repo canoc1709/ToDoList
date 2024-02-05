055b06e754987dda686e1b1fd5ab60359b9b56c6
for2024 <phamchung1.qtkd@gmail.com>
2024-02-06 00:13:18 +0700

    Add asyns function with GET method can save data when reload the webpage, but still can't display it

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,11 @@
-var ul = document.getElementById("ul");
+
 var input = document.getElementById("INP");
+var ul = document.getElementById("ul");
 function Input(event) {
     if(event.code === "Enter")
     {
         inputValue = document.getElementById("INP").value;
-        sendData();
+        sendData(inputValue);
         var newList = document.createElement("li");
         newList.textContent = inputValue;
         ul.appendChild(newList);
@@ -13,16 +14,23 @@ function Input(event) {
 }
 input.addEventListener("keypress", Input);
 
-function sendData() {
-    let inputText = document.getElementById("INP").value;
-    const response = fetch('/postData',{
+async function sendData(taskValue) {
+    const response = await fetch('http://127.0.0.1:5000/postData',{
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({data: inputText})
+        body: JSON.stringify({tasks: taskValue})
     })
-    console.log(response)
-    response.body = inputText
-    return response.body
+    const result = await response.json();
+    console.log("successful:", result);
 }
+
+async function getData(){
+    const response = await fetch('http://127.0.0.1:5000/getData');
+    const data = await response.json();
+    alert(data['tasks'])
+    
+}
+
+window.addEventListener("onload", getData())
